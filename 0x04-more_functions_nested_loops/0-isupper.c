4d10437e8666f481f19fa2787881a5a266c92f8f
ultron <mulerfrommarss@gmail.com>
2023-02-23 23:25:53 +0300

    check upper case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper - check for uppercase
+ * @c: the character to be checked
+ * Return: 1 if uppercase, else 0
+ */
+int _isupper(int c)
+{
+	return (c <= 'Z' && c >= 'A');
+}
